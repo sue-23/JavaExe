7d52a9fa7b1ead6eb7158c2082b262ce31cba98a
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 15:43:41 +0900

    'DataType.java' 변수 선언

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,27 @@
+package ch02.variable01;
+
+public class DataType {
+
+	public static void main(String[] args) {
+
+		int num =100;
+		double dnum = 3.14;
+		char ch = 'a';
+		String name ="조수연";
+		boolean isRun = true;
+		String str = "a";
+		
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(isRun);
+		System.out.println(str);
+		
+				
+				
+		
+		
+	}
+
+}
