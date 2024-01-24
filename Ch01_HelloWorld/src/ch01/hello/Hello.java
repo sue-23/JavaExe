aea223f83ff594ebf7469ee570c412f152e9b07d
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:34:17 +0900

    Hello suesuesue 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello sue");
+		System.out.println("Hello suesuesue");
+
 
 		
 
