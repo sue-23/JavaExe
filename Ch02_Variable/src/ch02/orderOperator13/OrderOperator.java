d267142eb670fde9da4c2c27b68b3b0a5ae017ac
Sue <navercloud.gmail.com>
2024-01-25 12:51:50 +0900

    연산자 우선순위 ()

---
+++ Ch02_Variable/src/ch02/orderOperator13/OrderOperator.java
@@ -0,0 +1,12 @@
+package ch02.orderOperator13;
+
+public class OrderOperator {
+
+	public static void main(String[] args) {
+		int result = 10 +50 *3;
+		System.out.println(result);
+		
+		int result1 = (10+50)*3;
+		System.out.println(result1);
+	}
+}
