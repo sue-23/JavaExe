c3f17383b2a676b06b758f6c590d51d958b4771f
Sue <navercloud.gmail.com>
2024-01-25 17:04:58 +0900

    설명 붙임

--- CH03_Control/src/ch03/dowhile09/DoWhileEx.java
+++ CH03_Control/src/ch03/dowhile09/DoWhileEx.java
@@ -6,11 +6,11 @@ public class DoWhileEx {
 		//while은 조건을 먼거 검사하고 실행하는 반면
 		//do-while은 1번은 무조건 실행후 조건 검사하여 반복 결정.
 		
-		int i =1;
+		int i =1;//초기값
 		do {
-			System.out.println(i);
-			i++;	
-		}while(i<=10);
+			System.out.println(i);//실행
+			i++;	//증가(변화)
+		}while(i<=10);//조건(언제까지)
 		
 		
 	}
