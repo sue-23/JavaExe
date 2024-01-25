a4998a8e6b7414c6ce6267f6ac29ab98475d610c
Sue <navercloud.gmail.com>
2024-01-25 14:45:54 +0900

    else if

---
+++ CH03_Control/src/ch03/contro03/IfEx.java
@@ -0,0 +1,28 @@
+package ch03.contro03;
+
+import java.util.Scanner;
+
+public class IfEx {
+
+	public static void main(String[] args) {
+
+		int score;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("0-100점수 입력:");
+		score = sc.nextInt();
+		
+		if(score>=90)
+			System.out.println("A");
+		else if(score>=80)
+			System.out.println("B");
+		else if(score>=70)
+			System.out.println("C");
+		else if(score>=60)
+			System.out.println("D");
+		else
+			System.out.println("F");
+		sc.close();
+	}
+
+}
